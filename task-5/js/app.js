2b03a2ef6ea86f00f1263ebfa20b82ab6ec6a010
musavirk <musavirmusthafa2001@gmail.com>
2023-05-13 20:52:32 +0530

    updated

--- task-5/js/app.js
+++ task-5/js/app.js
@@ -1,6 +1,58 @@
-alert("hello");
+/*alert("hello");
 console.log("one")
 console.info("two")
 console.warn("three")
 console.error("four")
 console.clear
+
+var firstName = "Ayisha";
+var lastName = "Misna";
+let age = "10";
+const sheIs = true;
+
+console.log(firstName)
+let num1 = 5 ;
+let num2 = 6 ;
+let sum = num1 + num2 ;
+alert(sum) 
+
+function findSum(n1, n2){
+     return n1 + n2 ;
+}
+ 
+let sum= findSum (3 , 5)
+ alert(sum)
+ console.log(findSum(10 ,9))
+ console.warn(findSum(3 , 4))
+let color= "red" ;
+
+ if (color === "green"){
+    console.log ("its okey");
+ }
+ else if( color === "orenge"){
+    console.log("its looking not safe");
+ }
+ else if (color === "red" ){
+    console.log("its danger");
+ }
+ else{
+    console.log("not sure")
+ }*/
+
+ const element1 = document.getElementById("Branch");
+ const element2 = document.getElementById("email");
+ console.log(element1)
+ console.log(element2)
+ const color = element1.value;
+ 
+ if (color === "green"){
+    console.log ("its okey");
+ }
+ else if( color === "orenge"){
+    console.log("its looking not safe");
+ }
+ else if (color === "red" ){
+    console.log("its danger");
+ }
+ else{
+    console.log("not sure")}
