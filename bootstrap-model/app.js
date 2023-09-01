38eebcfe4a45812a7bfc4a4a7279308ffd3c662a
musavirk <musavirmusthafa2001@gmail.com>
2023-09-01 16:15:13 +0530

    updated

---
+++ bootstrap-model/app.js
@@ -0,0 +1,62 @@
+// const app= vue.createApp({
+//     template:'<h2>I am the template</h2>'
+// })
+
+// app.mount('#navbar')
+
+// alert("hello")
+// console.log(1)
+// console.info(1)
+// console.warn(1)
+// console.error(5)
+// console.debug(10)
+
+// var a = 5
+// let b = "hi"
+// const c = false
+
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+
+// function findSum(n1,n2){
+//     return n1+n2
+
+// }
+
+// let sum = findSum(3,5)
+// alert(sum)
+
+// let result = sum*5
+// console.log(result)
+
+
+
+// alert(color)
+// function enterNumber(){
+//     let number = document.getElementById("number")
+//     number.style.backgroundColor="red"
+    // if ( number >= 10) {
+    //     alert('you are eligible')
+        
+    // }
+    // else{
+    //     alert("you are not eligible")
+    // }
+    
+// }
+
+
+
+let students ={ firstName:"musavir" ,
+            lastName:"musthafa"
+}
+students.age= 5
+// console.log(students)
+
+
+
+const student=["name",5,true ]
+console.log(typeof student[2])
